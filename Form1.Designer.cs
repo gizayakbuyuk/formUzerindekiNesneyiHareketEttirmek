67e8861c026d1eb1a2a51362b0e59aba15f9e06d
gizayakbuyuk <gizaykata@gmail.com>
2016-03-17 19:06:30 +0200

    Added files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace form_üzerindeki_nesneyi_hareket_ettirmek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.kare = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(167, 563);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 21);
+            this.label1.TabIndex = 1;
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            // 
+            // kare
+            // 
+            this.kare.BackColor = System.Drawing.Color.Blue;
+            this.kare.Location = new System.Drawing.Point(381, 347);
+            this.kare.Name = "kare";
+            this.kare.Size = new System.Drawing.Size(30, 30);
+            this.kare.TabIndex = 1;
+            this.kare.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 634);
+            this.Controls.Add(this.kare);
+            this.Controls.Add(this.label1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label kare;
+        private System.Windows.Forms.Timer timer1;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form_üzerindeki_nesneyi_hareket_ettirmek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:     label1.Left -= 5;  break;
+                case Keys.Right:    label1.Left += 5; break;
+                case Keys.Up:       label1.Top  -= 5; break;
+                case Keys.Down:     label1.Top  += 5; break;
+                default:    break;
+            }
+            
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int yk = e.X - label1.Width / 2;
+            if (sender is Label)
+                yk = label1.Left+(e.X - label1.Width / 2);
+
+            if(yk>=0 && yk<this.Width-label1.Width)
+                    label1.Left = yk;
+
+        }
+        string yön = "sağüst";
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            switch (yön)
+            {
+                case "sağalt": kare.Top += 8; kare.Left += 8; break;
+                case "sağüst": kare.Top -= 8; kare.Left += 8; break;
+                case "solalt": kare.Top += 8; kare.Left -= 8; break;
+                case "solüst": kare.Top -= 8; kare.Left -= 8; break;
+            }
+            int altcizgi = kare.Top + kare.Height;
+            int üstcizgi = kare.Top;
+            int solcizgi = kare.Left;
+            int sağcizgi = kare.Left + kare.Width;
+            
+            if (üstcizgi <= 0 && yön == "sağüst") yön = "sağalt";
+            else if (üstcizgi <= 0 && yön == "solüst") yön = "solalt";
+            else if (solcizgi <= 0 && yön == "solalt") yön = "sağalt";
+            else if (solcizgi <= 0 && yön == "solüst") yön = "sağüst";
+            else if (sağcizgi >= this.Width && yön == "sağüst") yön = "solüst";
+            else if (sağcizgi >= this.Width && yön == "sağalt") yön = "solalt";
+
+            if (altcizgi >= label1.Top)
+                if (solcizgi+kare.Width > label1.Left && sağcizgi-kare.Width-1 < label1.Left + label1.Width)
+                {
+                    if (yön == "solalt") yön = "solüst";
+                    else if (yön == "sağalt") yön = "sağüst";
+                }
+                else if (altcizgi >= label1.Top + label1.Height +1)
+                {
+                    timer1.Enabled = false;
+                    MessageBox.Show("Kaybettiniz!");
+                    yön = "sağalt";
+                    kare.Top = 20;
+                    kare.Left = 20;
+                    timer1.Enabled = true;
+                }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            label1.Top = this.Height - label1.Height - 50;
+            label1.Width = this.Width/2;
+        }
+    }
+}
